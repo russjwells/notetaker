d9fbf0d16e3f006fbe4e07d98dd5fc6f8b973818
Russ Wells <russ@russdigital.com>
2016-06-14 12:20:17 -0700

    componentWillReceiveProps and react router smoothness

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -18,19 +18,27 @@ var Profile = React.createClass({
 	},
 	componentDidMount: function(){
 		this.ref = new Firebase('https://notetaker-be568.firebaseio.com/');
-		var childRef = this.ref.child(this.props.params.username);
+		this.init(this.props.params.username);
+
+	},
+	componentWillReceiveProps: function(nextProps){
+		this.unbind('notes');
+		this.init(nextProps.params.username);
+	},
+	componentWillUnmount: function(){
+		this.unbind('notes');
+	},
+	init: function(username){
+		var childRef = this.ref.child(username);
 		this.bindAsArray(childRef, 'notes');
 
-		helpers.getGithubInfo(this.props.params.username)
+		helpers.getGithubInfo(username)
 			.then(function(data){
 				this.setState({
 					bio: data.bio,
 					repos: data.repos
 				})
 			}.bind(this))
-	},
-	componentWillUnmount: function(){
-		this.unbind('notes');
 	},
 	handleAddNote: function(newNote){
 		//update firebase with the newNote
--- public/bundle.js
+++ public/bundle.js
@@ -24520,18 +24520,25 @@
 		},
 		componentDidMount: function componentDidMount() {
 			this.ref = new Firebase('https://notetaker-be568.firebaseio.com/');
-			var childRef = this.ref.child(this.props.params.username);
+			this.init(this.props.params.username);
+		},
+		componentWillReceiveProps: function componentWillReceiveProps(nextProps) {
+			this.unbind('notes');
+			this.init(nextProps.params.username);
+		},
+		componentWillUnmount: function componentWillUnmount() {
+			this.unbind('notes');
+		},
+		init: function init(username) {
+			var childRef = this.ref.child(username);
 			this.bindAsArray(childRef, 'notes');
 
-			helpers.getGithubInfo(this.props.params.username).then(function (data) {
+			helpers.getGithubInfo(username).then(function (data) {
 				this.setState({
 					bio: data.bio,
 					repos: data.repos
 				});
 			}.bind(this));
-		},
-		componentWillUnmount: function componentWillUnmount() {
-			this.unbind('notes');
 		},
 		handleAddNote: function handleAddNote(newNote) {
 			//update firebase with the newNote
@@ -25486,7 +25493,7 @@
 		handleSubmit: function handleSubmit() {
 			var username = this.usernameRef.value;
 			this.usernameRef.value = '';
-			this.history.pushState(null, "profile/" + username);
+			this.history.pushState(null, "/profile/" + username);
 		},
 		render: function render() {
 			return React.createElement(
